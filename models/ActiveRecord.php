e0f1003b464229f2d8011a0a35f98434562a1acc
DevRenato <u18215194@gmail.com>
2023-02-02 19:12:48 -0500

    Active Record Finalizado

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -23,7 +23,97 @@
       return static::$alerts;
     }
 
-    //Consultas
+    //Consultas Definidas
+    public static function findAll(): array {
+      $query = "SELECT * FROM ". static::$table;
+      $result = self::consultSQL( $query );
+      return $result;
+    }
+    public static function findById( $id ): array {
+      $query = "SELECT * FROM ". static::$table . " WHERE id = {$id} LIMIT 1";
+      $result = self::consultSQL( $query );
+      return array_shift( $result );
+    }
+    public static function findOne( $column, $value ): array {
+      $query = "SELECT * FROM ". static::$table . " WHERE {$column} = '{$value}' LIMIT 1 ";
+      $result = self::consultSQL( $query );
+      return array_shift( $result );
+    }
+    public static function findLimit( $limit ): array {
+      $query = "SELECT * FROM ". static::$table . " ORDER BY id DESC LIMIT {$limit} ";
+      $result = self::consultSQL( $query );
+      return $result;
+    }
+    public static function findPaginate( $pages, $offset): array {
+      $query = "SELECT * FROM ". static::$table ." ORDER BY id DESC LIMIT {$pages} OFFSET {$offset} " ;
+      $resultado = self::consultSQL( $query) ;
+      return $resultado;
+    }
+    public static function findOrderBy( $column, $order ): array {
+      $query = "SELECT * FROM ". static::$table ." ORDER BY {$column} {$order}";
+      $resultado = self::consultSQL( $query );
+      return  $resultado;
+    }
+    public static function findOrderLimit( $column, $order, $limit ) {
+      $query = "SELECT * FROM ". static::$table ." ORDER BY {$column} {$order} LIMIT {$limit}";
+      $resultado = self::consultSQL($query);
+      return  $resultado;
+    }
+    public static function findOneMore( $data = [] ): array {
+      $query = "SELECT * FROM ". static::$table ." WHERE ";
+      foreach ($data as $key => $value) {
+        if ($key === array_key_last( $data )) {
+          $query .= " {$key} = '{$value}'";
+        } else {
+          $query .= " {$key} = '{$value}' AND ";
+        }
+      }
+      $resultado = self::consultSQL($query);
+      return  $resultado  ;
+    }
+    public static function findTotal( $column = '', $value = '' ): array {
+      $query = "SELECT COUNT(*) FROM ". static::$table;
+      if($column) {
+        $query .= " WHERE {$column} = {$value}";
+      }
+      $result = self::$database->query( $query );
+      $total = $result->fetch_array();
+
+      return array_shift($total);
+    }
+    public static function findTotalMore( $data = [] ) {
+      $query = "SELECT COUNT(*) FROM ". static::$table ." WHERE";
+      foreach ($data as $key => $value) {
+        if ($key === array_key_last( $data )) {
+          $query .= " {$key} = '{$value}'";
+        } else {
+          $query .= " {$key} = '{$value}' AND ";
+        }
+      }
+      $result = self::$database->query($query);
+      $total = $result->fetch_array();
+      return array_shift($total);
+    }
+    //Insertar en la Base de Datos
+    public static function consultSQL( $query ): array {
+      $result = self::$database->query( $query );
+
+      $container = [];
+      while ( $register = $result->fetch_assoc() ) {
+        $container[] = static::createObjects( $register );
+      }
+      $result->free();
+      return $container;
+    }
+    public static function createObjects( $register ): object {
+      $object = new static;
+      foreach ($register as $key => $value) {
+        if ( property_exists( $object, $key ) ) {
+          $object->$key = $value;
+        }
+      }
+      return $object;
+    }
 
     //CRUD
     public function save() {
@@ -33,11 +123,11 @@
       return $this->update();
     }
     public function insert(): array {
-      $attributes = [];
+      $attributes = $this->sanitizeAttribute();
 
-      $query = 'INSERT INTO '. static::$table . '( ';
-      $query += join(', ', array_keys( $attributes ));
-      $query += ' ) VALUES ( ';
+      $query = "INSERT INTO ". static::$table . "( ";
+      $query += join(", ", array_keys( $attributes ));
+      $query += " ) VALUES ( ";
       $query += join("', ", array_values( $attributes ) );
       $query += " ')";
       $result = self::$database->query( $query );
@@ -45,7 +135,7 @@
       return [ 'result'=> $result, 'id' => self::$database->insert_id ];
     }
     public function update(): bool {
-      $attributes = [];
+      $attributes = $this->sanitizeAttribute();
 
       $values = [];
       foreach ($attributes as $key => $value) {
@@ -55,15 +145,15 @@
       $query = 'UPDATE '. static::$table . ' SET ';
       $query += join(', ', $values);
       $query += " WHERE id = '". self::$database->escape_string( $this->id ). "' ";
-      $query += " LIMIT 1 ";
+      $query += "LIMIT 1 ";
       $result = self::$database->query( $query );
 
       return $result;
     }
     public function delete(): bool {
-      $query = 'DELETE FROM '. static::$table . ' ';
-      $query += 'WHERE id = '. self::$database->escape_string( $this->id ) . ' ';
-      $query += 'LIMIT 1';
+      $query = "DELETE FROM ". static::$table . " ";
+      $query += "WHERE id = ". self::$database->escape_string( $this->id ) . " ";
+      $query += "LIMIT 1";
       $result = self::$database->query( $query );
 
       return $result;
@@ -84,7 +174,7 @@
       }
       return $sanitized;
     }
-    public function syncUp( $args = [] ): void {
+    public function synchronize( $args = [] ): void {
       foreach ($args as $key => $value) {
         if (property_exists( $this, $key) && !is_null( $value) ) {
           $this->$key = $value;
